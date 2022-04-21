27ab144b1fa8e48f02869c03e5d12a2d2d4de842
Quernest <a.zhit4enko@gmail.com>
2022-04-21 20:26:49 +0300

    add app

---
+++ example/src/app.tsx
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+
+import { useModal } from '../../src';
+
+import {
+  SimpleDialog,
+  NestedDialog,
+  ConfirmationDialog,
+  SimpleModal,
+  TransitionModal,
+} from './components';
+
+const App = () => {
+  const { showModal } = useModal();
+
+  const handleOpenConfirmationDialog = () => {
+    let timeoutId;
+
+    const modal = showModal(ConfirmationDialog, {
+      title: 'Hello World',
+      description: 'description text',
+      onConfirm: () => {
+        modal.hide();
+      },
+      onCancel: () => {
+        modal.hide();
+      },
+      TransitionProps: {
+        onExited: () => {
+          // =========================
+          // ⚠️ Be careful with setImmediate, setInterval and setTimeout
+          // ⚠️ don't forget to clean up id after closing dialog.
+          // ⚠️ use `onExited` or `onClose` callback for this.
+          // =========================
+          clearTimeout(timeoutId);
+        },
+      },
+    });
+
+    // Updating props if needed.
+    timeoutId = setTimeout(() => {
+      modal.update({
+        title: 'Updated hello world',
+        description: 'updated description text',
+      });
+    }, 1000);
+  };
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item>
+        <Button
+          variant="contained"
+          onClick={() => showModal(SimpleDialog)}
+          color="primary"
+        >
+          simple dialog
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          onClick={() => showModal(NestedDialog)}
+          color="primary"
+        >
+          nested dialog
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          onClick={handleOpenConfirmationDialog}
+          color="primary"
+        >
+          confirmation dialog
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          // @see https://github.com/Quernest/mui-modal-provider/issues/14
+          onClick={() =>
+            showModal(SimpleModal, undefined, { destroyOnClose: true })
+          }
+          color="primary"
+        >
+          simple modal
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          onClick={() => showModal(TransitionModal)}
+          color="primary"
+        >
+          transition modal
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default App;
