0927e91e3c5b9d75aeccc7e6209af4395a485c5a
Quernest <a.zhit4enko@gmail.com>
2023-07-08 16:29:17 +0300

    #77 added named export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export { default, ModalProviderProps } from './modal-provider';
+export {
+  default,
+  default as ModalProvider,
+  ModalProviderProps,
+} from './modal-provider';
 export { default as useModal, UseModalOptions } from './use-modal';
 export * from './types';
