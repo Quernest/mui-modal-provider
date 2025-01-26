aae4dea36647945f316eb077c1abcbdbdfdc5289
Quernest <a.zhit4enko@gmail.com>
2025-01-26 22:28:31 +0200

    #99 update packages versions (react to 19)

--- src/use-modal.test.tsx
+++ src/use-modal.test.tsx
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react';
 
 import { ModalProviderWrapper as wrapper } from './test-utils';
 import Modal from './test-utils/modal';
